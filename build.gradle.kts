3ab785245ce5d0bdbdd0510209b4747752d31076
Giwan Kim <giwan.kim@gmail.com>
2024-07-15 09:24:01 +0900

    feat: add naive TwoSum implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.giwankim"
@@ -11,9 +12,19 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.assertj:assertj-bom:3.26.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+spotless {
+    java {
+        removeUnusedImports("google-java-format")
+        googleJavaFormat().reflowLongStrings().reorderImports(true)
+        formatAnnotations()
+    }
 }
--- src/test/java/com/giwankim/leetcode/TwoSumTest.java
+++ src/test/java/com/giwankim/leetcode/TwoSumTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.giwankim.leetcode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TwoSumTest {
-  
+  @ParameterizedTest(name = "{2} sums to {1} in {0}")
+  @MethodSource("baseTestCases")
+  void should_return_indices(int[] nums, int target, int[] expected) {
+    TwoSum subject = new TwoSum();
+    int[] result = subject.twoSum(nums, target);
+    assertThat(result).containsExactlyInAnyOrder(expected);
+  }
+
+  private static Stream<Arguments> baseTestCases() {
+    return Stream.of(
+        Arguments.of(new int[] {2, 7, 11, 15}, 9, new int[] {0, 1}),
+        Arguments.of(new int[] {3, 2, 4}, 6, new int[] {1, 2}),
+        Arguments.of(new int[] {3, 3}, 6, new int[] {0, 1}));
+  }
 }
