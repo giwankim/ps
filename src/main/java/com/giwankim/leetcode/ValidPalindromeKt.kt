440b71e4e6b96c6c1f237ac9eb428feb9aac628c
Giwan Kim <giwan.kim@gmail.com>
2024-08-13 09:50:59 +0900

    feat: valid palindrome in kotlin
    https://leetcode.com/problems/valid-palindrome/description/

--- src/main/java/com/giwankim/leetcode/ValidPalindromeKt.kt
+++ src/main/java/com/giwankim/leetcode/ValidPalindromeKt.kt
@@ -1,5 +1,20 @@
 package com.giwankim.leetcode
 
 class ValidPalindromeKt {
-    fun isPalindrome(s: String): Boolean = false
+    fun isPalindrome(s: String): Boolean {
+        var start = 0
+        var end = s.length - 1
+        while (start < end) {
+            when {
+                !Character.isLetterOrDigit(s[start]) -> start += 1
+                !Character.isLetterOrDigit(s[end]) -> end -= 1
+                Character.toLowerCase(s[start]) != Character.toLowerCase(s[end]) -> return false
+                else -> {
+                    start += 1
+                    end -= 1
+                }
+            }
+        }
+        return true
+    }
 }
