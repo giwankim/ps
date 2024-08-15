ee3c8cadea7e0947eae36b5f687bad0f7766e13c
Giwan Kim <giwan.kim@gmail.com>
2024-08-15 10:38:05 +0900

    feat: 삼각 달팽이
    https://school.programmers.co.kr/learn/courses/30/lessons/68645

--- src/main/java/com/giwankim/leetcode/ReverseString.java
+++ src/main/java/com/giwankim/leetcode/ReverseString.java
@@ -3,7 +3,7 @@ package com.giwankim.leetcode;
 public class ReverseString {
   public void reverseString(char[] s) {
     int start = 0;
-    int end  = s.length - 1;
+    int end = s.length - 1;
     while (start < end) {
       char temp = s[start];
       s[start] = s[end];
--- src/test/java/com/giwankim/programmers/삼각_달팽이Test.java
+++ src/test/java/com/giwankim/programmers/삼각_달팽이Test.java
@@ -1,12 +1,11 @@
 package com.giwankim.programmers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
 class 삼각_달팽이Test {
@@ -20,13 +19,13 @@ class 삼각_달팽이Test {
 
   static Stream<Arguments> testCases() {
     return Stream.of(
-        Arguments.of(1, new int[]{1}),
-        Arguments.of(2, new int[]{1, 2, 3}),
-        Arguments.of(3, new int[]{1, 2, 6, 3, 4, 5}),
-        Arguments.of(4, new int[]{1, 2, 9, 3, 10, 8, 4, 5, 6, 7}),
-        Arguments.of(5, new int[]{1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9}),
+        Arguments.of(1, new int[] {1}),
+        Arguments.of(2, new int[] {1, 2, 3}),
+        Arguments.of(3, new int[] {1, 2, 6, 3, 4, 5}),
+        Arguments.of(4, new int[] {1, 2, 9, 3, 10, 8, 4, 5, 6, 7}),
+        Arguments.of(5, new int[] {1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9}),
         Arguments.of(
             6,
-            new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11}));
+            new int[] {1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11}));
   }
 }
