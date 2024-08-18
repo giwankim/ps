1a1af7cf76897f230215306ce4074a5401a6c101
Giwan Kim <giwan.kim@gmail.com>
2024-08-18 16:35:55 +0900

    feat: 거리두기 확인하기
    https://school.programmers.co.kr/learn/courses/30/lessons/81302

---
+++ src/test/java/com/giwankim/programmers/거리두기_확인하기Test.java
@@ -0,0 +1,30 @@
+package com.giwankim.programmers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class 거리두기_확인하기Test {
+  @ParameterizedTest
+  @MethodSource
+  void solution(String[][] places, int[] expected) {
+    int[] actual = new 거리두기_확인하기().solution(places);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> solution() {
+    return Stream.of(
+        Arguments.of(
+            new String[][]{
+                {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
+                {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+                {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
+                {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
+                {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}},
+            new int[]{1, 0, 1, 1, 1}));
+  }
+}
