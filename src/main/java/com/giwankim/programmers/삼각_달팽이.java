be15ce3eca27cd9bbcb9fb6352e262bbe18ceab3
Giwan Kim <giwan.kim@gmail.com>
2024-08-18 10:38:09 +0900

    feat: 삼각_달팽이 조금더 효율적인 풀이
    https://leetcode.com/problems/two-sum/description/

--- src/main/java/com/giwankim/programmers/삼각_달팽이.java
+++ src/main/java/com/giwankim/programmers/삼각_달팽이.java
@@ -2,71 +2,39 @@ package com.giwankim.programmers;
 
 @SuppressWarnings("NonAsciiCharacters")
 public class 삼각_달팽이 {
+  private static final int[] dx = {0, 1, -1};
+  private static final int[] dy = {1, 0, -1};
+
   public int[] solution(int n) {
     int[][] a = new int[n][n];
-    int curr = 1;
+    int v = 1;
     int x = 0;
     int y = 0;
+    int d = 0;
 
     while (true) {
-      // 아래로 이동
-      while (true) {
-        a[y][x] = curr++;
-        if (y + 1 == n || a[y + 1][x] != 0) {
+      a[y][x] = v++;
+      int nx = x + dx[d];
+      int ny = y + dy[d];
+      if (nx == n || ny == n || nx == -1 || ny == -1 || a[ny][nx] != 0) {
+        d = (d + 1) % 3;
+        nx = x + dx[d];
+        ny = y + dy[d];
+        if (nx == n || ny == n || nx == -1 || ny == -1 || a[ny][nx] != 0) {
           break;
         }
-        y += 1;
       }
-      if (x + 1 == n || a[y][x + 1] != 0) {
-        break;
-      }
-      x += 1;
-
-      // 오른쪽으로 이동
-      while (true) {
-        a[y][x] = curr++;
-        if (x + 1 == n || a[y][x + 1] != 0) {
-          break;
-        }
-        x += 1;
-      }
-      if (a[y - 1][x - 1] != 0) {
-        break;
-      }
-      x -= 1;
-      y -= 1;
-
-      // 대각선으로 이동
-      while (true) {
-        a[y][x] = curr++;
-        if (a[y - 1][x - 1] != 0) {
-          break;
-        }
-        x -= 1;
-        y -= 1;
-      }
-      if (y + 1 == n || a[y + 1][x] != 0) {
-        break;
-      }
-      y += 1;
+      x = nx;
+      y = ny;
     }
 
+    int[] result = new int[v - 1];
     int index = 0;
-    int[] result = new int[n * (n + 1) / 2];
     for (int i = 0; i < n; i++) {
       for (int j = 0; j <= i; j++) {
         result[index++] = a[i][j];
       }
     }
     return result;
-  }
-
-  public static void print(int[][] a, int n) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        System.out.printf("%d, ", a[i][j]);
-      }
-      System.out.println();
-    }
   }
 }
