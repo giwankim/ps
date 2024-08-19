7e4d46023aa82382a41787123bd0619b6b523e69
Giwan Kim <giwan.kim@gmail.com>
2024-08-19 12:51:59 +0900

    feat: most common word
    https://leetcode.com/problems/most-common-word/description/

--- src/test/java/com/giwankim/programmers/거리두기_확인하기Test.java
+++ src/test/java/com/giwankim/programmers/거리두기_확인하기Test.java
@@ -3,7 +3,6 @@ package com.giwankim.programmers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,13 +19,13 @@ class 거리두기_확인하기Test {
   private static Stream<Arguments> solution() {
     return Stream.of(
         Arguments.of(
-            new String[][]{
-                {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
-                {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
-                {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
-                {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
-                {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}
+            new String[][] {
+              {"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"},
+              {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},
+              {"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
+              {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},
+              {"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}
             },
-            new int[]{1, 0, 1, 1, 1}));
+            new int[] {1, 0, 1, 1, 1}));
   }
 }
