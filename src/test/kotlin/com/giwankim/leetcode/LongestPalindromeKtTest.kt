eecdeda9da0c38169c15a03741c86e9153188f41
Giwan Kim <giwan.kim@gmail.com>
2024-08-20 15:42:49 +0900

    feat: longest palindrome substring in Kotlin
    https://leetcode.com/problems/longest-palindromic-substring/description/

---
+++ src/test/kotlin/com/giwankim/leetcode/LongestPalindromeKtTest.kt
@@ -0,0 +1,30 @@
+package com.giwankim.leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class LongestPalindromeKtTest {
+    @ParameterizedTest
+    @MethodSource
+    fun longestPalindrome(
+        s: String,
+        expected: String,
+    ) {
+        val actual = LongestPalindromeKt().longestPalindrome(s)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun longestPalindrome(): List<Arguments> =
+            listOf(
+                Arguments.of("a", "a"),
+                Arguments.of("ab", "a"),
+                Arguments.of("babad", "bab"),
+                Arguments.of("cbbd", "bb"),
+                Arguments.of("dcbabcdd", "dcbabcd"),
+            )
+    }
+}
