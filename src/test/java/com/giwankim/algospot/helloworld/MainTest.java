509309cf3bc9bbf6c4e3385068c4b763d2a061e7
Giwan Kim <giwan.kim@gmail.com>
2024-08-23 10:02:47 +0900

    feat: algospot hello world
    https://algospot.com/judge/problem/read/HELLOWORLD

---
+++ src/test/java/com/giwankim/algospot/helloworld/MainTest.java
@@ -0,0 +1,42 @@
+package com.giwankim.algospot.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+  @Test
+  void helloWorld() {
+    String input = """
+        5
+        World
+        Algospot
+        Illu
+        Jullu
+        Kodori
+        """;
+    String expected = """
+        Hello, World!
+        Hello, Algospot!
+        Hello, Illu!
+        Hello, Jullu!
+        Hello, Kodori!
+        """;
+    InputStream is = System.in;
+    PrintStream ps = System.out;
+    System.setIn(new ByteArrayInputStream(input.getBytes()));
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+    try {
+      Main.main(new String[0]);
+      assertThat(out).hasToString(expected);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } finally {
+      System.setIn(is);
+      System.setOut(ps);
+    }
+  }
+}
