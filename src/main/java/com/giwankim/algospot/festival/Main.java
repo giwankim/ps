0e355353a5fef58b57edc1aee2a41ff7858c9ca0
Giwan Kim <giwan.kim@gmail.com>
2024-08-23 11:48:21 +0900

    feat: algospot festival
    https://algospot.com/judge/problem/read/FESTIVAL

---
+++ src/main/java/com/giwankim/algospot/festival/Main.java
@@ -0,0 +1,48 @@
+package com.giwankim.algospot.festival;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter pw = new PrintWriter(System.out);
+
+    int C = Integer.parseInt(br.readLine());
+    while (C-- > 0) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int N = Integer.parseInt(st.nextToken());
+      int L = Integer.parseInt(st.nextToken());
+      List<Integer> costs = new ArrayList<>();
+      st = new StringTokenizer(br.readLine());
+      for (int i = 0; i < N; i++) {
+        costs.add(Integer.parseInt(st.nextToken()));
+      }
+      pw.printf("%.11f%n", festival(N, L, costs));
+    }
+
+    br.close();
+    pw.close();
+  }
+
+  private static double festival(int N, int L, List<Integer> costs) {
+    double minCost = Double.MAX_VALUE;
+    for (int i = 0; i <= N - L; i++) {
+      int sum = 0;
+      for (int j = i; j < N; j++) {
+        sum += costs.get(j);
+        int len = j - i + 1;
+        if (len < L) {
+          continue;
+        }
+        minCost = Math.min(minCost, (double) sum / len);
+      }
+    }
+    return minCost;
+  }
+}
