d8fa4dca10a03c0835a70346cd053b257f8516f6
Giwan Kim <giwan.kim@gmail.com>
2024-08-29 10:54:31 +0900

    refactor: AoC 2022 Day 1 partition from quicksort
    amortized time complexity O(n) if implemented efficiently
    
    https://adventofcode.com/2022/day/1

--- src/main/kotlin/aoc/aoc2022/Day01.kt
+++ src/main/kotlin/aoc/aoc2022/Day01.kt
@@ -1,7 +1,6 @@
 package aoc.aoc2022
 
 import java.io.File
-import java.util.PriorityQueue
 
 fun part1(input: String): Int {
     val data = parseInput(input)
@@ -17,14 +16,24 @@ private fun topNElves(
     data: List<List<Int>>,
     n: Int,
 ): Int {
-    val best = PriorityQueue<Int>()
-    for (calories in data.map { it.sum() }) {
-        best.add(calories)
-        if (best.size > n) {
-            best.poll()
+    fun findTopN(
+        n: Int,
+        elements: List<Int>,
+    ): List<Int> {
+        if (elements.size == n) {
+            return elements
+        }
+        val x = elements.random()
+        val small = elements.filter { it < x }
+        val equal = elements.filter { it == x }
+        val big = elements.filter { it > x }
+        return when {
+            big.size >= n -> findTopN(n, big)
+            big.size + equal.size >= n -> (big + equal).take(n)
+            else -> findTopN(n - big.size - equal.size, small) + equal + big
         }
     }
-    return best.sum()
+    return findTopN(n, data.map { it.sum() }).sum()
 }
 
 private fun parseInput(input: String) = input.split("\n\n").map { elf -> elf.lines().map { it.toInt() } }
