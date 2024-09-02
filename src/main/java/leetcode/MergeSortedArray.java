a789c8130c3b4d02b8be3ec36728a6fbbe4f6c8d
Giwan Kim <giwan.kim@gmail.com>
2024-09-02 20:10:44 +0900

    refactor: LeetCode 88. Merge Sorted Array slightly more elegant solution
    https://leetcode.com/problems/merge-sorted-array/description/

--- src/main/java/leetcode/MergeSortedArray.java
+++ src/main/java/leetcode/MergeSortedArray.java
@@ -1,34 +1,29 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MergeSortedArray {
   public void merge(int[] nums1, int m, int[] nums2, int n) {
-    List<Integer> result = new ArrayList<>();
-
-    int i = 0;
-    int j = 0;
-    while (i < m && j < n) {
-      if (nums1[i] <= nums2[j]) {
-        result.add(nums1[i]);
-        i += 1;
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] >= nums2[j]) {
+        nums1[k] = nums1[i];
+        i -= 1;
       } else {
-        result.add(nums2[j]);
-        j += 1;
+        nums1[k] = nums2[j];
+        j -= 1;
       }
+      k -= 1;
     }
-    while (i < m) {
-      result.add(nums1[i]);
-      i += 1;
+    while (i >= 0) {
+      nums1[k] = nums1[i];
+      i -= 1;
+      k -= 1;
     }
-    while (j < n) {
-      result.add(nums2[j]);
-      j += 1;
-    }
-
-    for (int k = 0; k < result.size(); k++) {
-      nums1[k] = result.get(k);
+    while (j >= 0) {
+      nums1[k] = nums2[j];
+      j -= 1;
+      k -= 1;
     }
   }
 }
