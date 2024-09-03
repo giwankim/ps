4c496d4b493b139cc4a45a35da8101365d3641a9
Giwan Kim <giwan.kim@gmail.com>
2024-09-03 10:52:38 +0900

    feat: Capital Tour naive O(N^2)

---
+++ src/main/java/learn/CapitalTour.java
@@ -0,0 +1,42 @@
+package learn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CapitalTour {
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+         PrintWriter pw = new PrintWriter(System.out);) {
+      capitalTour(r, pw);
+    }
+  }
+
+  public static void capitalTour(BufferedReader r, PrintWriter pw) throws IOException {
+    StringTokenizer st = new StringTokenizer(r.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+    int[] line1 = new int[n];
+    int[] line2 = new int[n];
+
+    st = new StringTokenizer(r.readLine());
+    for (int i = 0; i < n; i++) {
+      line1[i] = Integer.parseInt(st.nextToken());
+    }
+
+    st = new StringTokenizer(r.readLine());
+    for (int i = 0; i < n; i++) {
+      line2[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (line1[i] + line2[j] == k) {
+          pw.println(line1[i] + " " + line2[j]);
+        }
+      }
+    }
+  }
+}
---
+++ src/test/java/learn/CapitalTourTest.java
@@ -0,0 +1,28 @@
+package learn;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.*;
+
+import org.junit.jupiter.api.Test;
+
+class CapitalTourTest {
+  @Test
+  void capitalTour() throws IOException {
+    String input = """
+        4 20
+        4 7 12 15
+        6 8 14 17
+        """;
+    String expected = """
+        12 8
+        """;
+    BufferedReader r = new BufferedReader(new StringReader(input));
+    StringWriter out = new StringWriter();
+    PrintWriter pw = new PrintWriter(out);
+
+    CapitalTour.capitalTour(r, pw);
+
+    assertThat(out).hasToString(expected);
+  }
+}
