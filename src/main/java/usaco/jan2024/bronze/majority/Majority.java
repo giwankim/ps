ed436a53eebc740bd5e40ede21fc47be5e371e6e
Giwan Kim <giwan.kim@gmail.com>
2024-09-03 09:47:34 +0900

    refactor: USACO 2024 January Contest, Bronze. Problem 1. Majority Opinion
    TreeSet을 사용하여 정렬된 결과물을 저장하던 것을 문제의 제약 조건을 활용해 그냥 array에 저장했다.
    
    https://usaco.org/index.php?page=viewproblem2&cpid=1371

--- src/main/java/usaco/jan2024/bronze/majority/Majority.java
+++ src/main/java/usaco/jan2024/bronze/majority/Majority.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Majority {
   public static void main(String[] args) throws IOException {
@@ -20,31 +19,39 @@ public class Majority {
     while (T-- > 0) {
       int N = Integer.parseInt(r.readLine());
 
+      boolean[] result = new boolean[N + 1];
+
       int[] hay = new int[N];
       StringTokenizer st = new StringTokenizer(r.readLine());
       for (int i = 0; i < N; i++) {
         hay[i] = Integer.parseInt(st.nextToken());
       }
 
-      SortedSet<Integer> result = new TreeSet<>();
-
       for (int i = 0; i + 1 < N; i++) {
         if (hay[i] == hay[i + 1]) {
-          result.add(hay[i]);
+          result[hay[i]] = true;
         }
       }
-
       for (int i = 0; i + 2 < N; i++) {
         if (hay[i] == hay[i + 2]) {
-          result.add(hay[i]);
+          result[hay[i]] = true;
         }
       }
 
-      if (result.isEmpty()) {
-        pw.println(-1);
-      } else {
-        pw.println(result.stream().sorted().map(String::valueOf).collect(Collectors.joining(" ")));
+      boolean printed = false;
+      for (int i = 1; i <= N; i++) {
+        if (result[i]) {
+          if (printed) {
+            pw.print(" ");
+          }
+          pw.print(i);
+          printed = true;
+        }
       }
+      if (!printed) {
+        pw.print(-1);
+      }
+      pw.println();
     }
   }
 }
