803da7bd64686fb13fc56aac36e75fe8df96a895
Giwan Kim <giwan.kim@gmail.com>
2024-09-04 09:44:16 +0900

    chore: add scaffolding for USACO 2023 February Contest, Bronze Problem 1. Hungry Cow and 2023 January, Bronze Problem 1. Leaders
    https://usaco.org/index.php?page=viewproblem2&cpid=1299
    
    https://usaco.org/index.php?page=viewproblem2&cpid=1275

---
+++ src/main/java/usaco/jan2023/bronze/leaders/Leaders.java
@@ -0,0 +1,19 @@
+package usaco.jan2023.bronze.leaders;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Leaders {
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out)) {
+      leaders(r, pw);
+    }
+  }
+
+  public static void leaders(BufferedReader r, PrintWriter pw) {
+    throw new UnsupportedOperationException("Leaders#leaders not implemented yet!");
+  }
+}
