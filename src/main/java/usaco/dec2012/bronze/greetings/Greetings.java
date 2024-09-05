eb256f2eb5b4f0c9fb88488c247c77e16071889a
Giwan Kim <giwan.kim@gmail.com>
2024-09-05 13:32:20 +0900

    chore: scaffolding for USACO 2012 December contest, Bronze Problem1. Meet and Greet
    https://usaco.org/index.php?page=viewproblem2&cpid=205

---
+++ src/main/java/usaco/dec2012/bronze/greetings/Greetings.java
@@ -0,0 +1,19 @@
+package usaco.dec2012.bronze.greetings;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Greetings {
+  public static void main(String[] args) throws IOException {
+    try (BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out)) {
+      greetings(r, pw);
+    }
+  }
+
+  public static void greetings(BufferedReader r, PrintWriter pw) {
+    throw new UnsupportedOperationException("Greetings#greetings not implemented yet!");
+  }
+}
