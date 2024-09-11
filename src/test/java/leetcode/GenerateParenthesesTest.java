fbf3f13f960bbf3ac8887f9717487ac55940fe5c
Giwan Kim <giwan.kim@gmail.com>
2024-09-12 08:26:37 +0900

    feat: LeetCode 22. Generate Parentheses
    https://leetcode.com/problems/generate-parentheses/description/

---
+++ src/test/java/leetcode/GenerateParenthesesTest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class GenerateParenthesesTest {
+  @ParameterizedTest
+  @MethodSource
+  void generateParentheses(int n, List<String> expected) {
+    List<String> actual = new GenerateParentheses().generateParenthesis(n);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> generateParentheses() {
+    return Stream.of(
+        Arguments.of(3, List.of("((()))", "(()())", "(())()", "()(())", "()()()")),
+        Arguments.of(1, List.of("()")));
+  }
+}
