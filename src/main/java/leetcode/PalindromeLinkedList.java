20ea74a3f25cb6351d5924033cb297c7e844fdb9
Giwan Kim <giwan.kim@gmail.com>
2024-09-15 11:16:57 +0900

    feat: LeetCode 234. Palindrome Linked List implemented using deque
    https://leetcode.com/problems/palindrome-linked-list/

--- src/main/java/leetcode/PalindromeLinkedList.java
+++ src/main/java/leetcode/PalindromeLinkedList.java
@@ -1,23 +1,23 @@
 package leetcode;
 
 import datatype.ListNode;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Objects;
 
 public class PalindromeLinkedList {
   public boolean isPalindrome(ListNode head) {
-    Deque<Integer> stack = new ArrayDeque<>();
+    Deque<Integer> deque = new ArrayDeque<>();
     ListNode node = head;
     while (node != null) {
-      stack.push(node.val);
+      deque.push(node.val);
       node = node.next;
     }
-    node = head;
-    while (node != null) {
-      if (node.val != stack.pop()) {
+    while (deque.size() > 1) {
+      if (!Objects.equals(deque.pollFirst(), deque.pollLast())) {
         return false;
       }
-      node = node.next;
     }
     return true;
   }
