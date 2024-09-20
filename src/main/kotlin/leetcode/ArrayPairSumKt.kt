3a3f73bef5f0e9a67ee9b5c3bcc9edd7a3248bb7
Giwan Kim <giwan.kim@gmail.com>
2024-09-20 11:24:56 +0900

    feat: LeetCode 561 Array Partition; Kotlin
    https://leetcode.com/problems/array-partition/description/

---
+++ src/main/kotlin/leetcode/ArrayPairSumKt.kt
@@ -0,0 +1,14 @@
+package leetcode
+
+class ArrayPairSumKt {
+    fun arrayPairSum(nums: IntArray): Int {
+        nums.sort()
+        var ans = 0
+        for ((i, n) in nums.withIndex()) {
+            if (i % 2 == 0) {
+                ans += n
+            }
+        }
+        return ans
+    }
+}
---
+++ src/test/kotlin/leetcode/ArrayPairSumKtTest.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class ArrayPairSumKtTest {
+    @ParameterizedTest
+    @MethodSource
+    fun arrayPairSum(
+        nums: IntArray,
+        expected: Int,
+    ) {
+        val actual = ArrayPairSumKt().arrayPairSum(nums)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun arrayPairSum(): List<Arguments> =
+            listOf(
+                Arguments.of(intArrayOf(1, 4, 3, 2), 4),
+                Arguments.of(intArrayOf(6, 2, 6, 5, 1, 2), 9),
+            )
+    }
+}
