52314c6428ccc1e6d1da0e08d30ce4e5366b2b88
Giwan Kim <giwan.kim@gmail.com>
2024-09-22 08:49:32 +0900

    feat: LeetCode 121. Best Time to Buy and Sell Stock; Kotlin
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

--- src/test/java/leetcode/MaxProfitTest.java
+++ src/test/java/leetcode/MaxProfitTest.java
@@ -1,12 +1,11 @@
 package leetcode;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class MaxProfitTest {
   @ParameterizedTest
@@ -18,8 +17,6 @@ class MaxProfitTest {
 
   private static Stream<Arguments> maxProfit() {
     return Stream.of(
-        Arguments.of(new int[]{7, 1, 5, 3, 6, 4}, 5),
-        Arguments.of(new int[]{7, 6, 4, 3, 1}, 0)
-    );
+        Arguments.of(new int[] {7, 1, 5, 3, 6, 4}, 5), Arguments.of(new int[] {7, 6, 4, 3, 1}, 0));
   }
 }
