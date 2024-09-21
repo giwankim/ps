ec2719fdc831b344c47f79f69b170b7702b11cf8
Giwan Kim <giwan.kim@gmail.com>
2024-09-22 08:49:32 +0900

    feat: LeetCode 121. Best Time to Buy and Sell Stock; Kotlin
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

---
+++ src/main/kotlin/leetcode/MaxProfitKt.kt
@@ -0,0 +1,13 @@
+package leetcode
+
+class MaxProfitKt {
+    fun maxProfit(prices: IntArray): Int {
+        var maxProfit = 0
+        var minPrice = Int.MAX_VALUE
+        for (price in prices) {
+            maxProfit = maxProfit.coerceAtLeast(price - minPrice)
+            minPrice = minPrice.coerceAtMost(price)
+        }
+        return maxProfit
+    }
+}
