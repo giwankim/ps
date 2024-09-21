6fa1668359240df19527a42fd72952fd66d874a8
Giwan Kim <giwan.kim@gmail.com>
2024-09-21 17:47:15 +0900

    feat: LeetCode 121. Best Time to Buy and Sell Stock
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

---
+++ src/main/java/leetcode/MaxProfit.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class MaxProfit {
+  public int maxProfit(int[] prices) {
+    int minPrice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+    for (int price : prices) {
+      maxProfit = Math.max(maxProfit, price - minPrice);
+      minPrice = Math.min(price, minPrice);
+    }
+    return maxProfit;
+  }
+}
