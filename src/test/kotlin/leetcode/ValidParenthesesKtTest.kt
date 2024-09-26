04ebcb43003cac5c18bae25d975c973085d47fba
Giwan Kim <giwan.kim@gmail.com>
2024-09-27 08:53:05 +0900

    feat: Leetcode 20. Valid Parentheses in Kotlin
    https://leetcode.com/problems/valid-parentheses/

---
+++ src/test/kotlin/leetcode/ValidParenthesesKtTest.kt
@@ -0,0 +1,30 @@
+package leetcode
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class ValidParenthesesKtTest {
+    @ParameterizedTest
+    @MethodSource
+    fun isValid(
+        s: String,
+        expected: Boolean,
+    ) {
+        val actual = ValidParenthesesKt().isValid(s)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun isValid(): List<Arguments> =
+            listOf(
+                Arguments.of("()", true),
+                Arguments.of("()[]{}", true),
+                Arguments.of("(]", false),
+                Arguments.of("([])", true),
+                Arguments.of("]", false),
+            )
+    }
+}
