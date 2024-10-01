8dc764219c2bbb909a2845643fa5d482bab44c31
Giwan Kim <giwan.kim@gmail.com>
2024-10-02 07:56:30 +0900

    feat: LeetCode 2446. Determine if Two Events Have Conflict; optimized
    https://leetcode.com/problems/determine-if-two-events-have-conflict/description/

--- src/main/java/leetcode/TwoEventsHaveConflict.java
+++ src/main/java/leetcode/TwoEventsHaveConflict.java
@@ -1,18 +1,7 @@
 package leetcode;
 
-import java.util.Arrays;
-
 public class TwoEventsHaveConflict {
   public boolean haveConflict(String[] event1, String[] event2) {
-    String[][] events = {event1, event2};
-    Arrays.sort(
-        events,
-        (a, b) -> {
-          if (a[0].compareTo(b[0]) == 0) {
-            return a[1].compareTo(b[1]);
-          }
-          return a[0].compareTo(b[0]);
-        });
-    return events[0][1].compareTo(events[1][0]) >= 0;
+    return event1[0].compareTo(event2[1]) <= 0 && event2[0].compareTo(event1[1]) <= 0;
   }
 }
