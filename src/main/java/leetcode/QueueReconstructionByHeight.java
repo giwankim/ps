51dca16fe8ae7cc0d8b8b659618ea443a85b53ba
Giwan Kim <giwan.kim@gmail.com>
2024-10-03 20:16:08 +0900

    feat: LeetCode 406. Queue Reconstruction by Height; using sort method
    https://leetcode.com/problems/queue-reconstruction-by-height/description/

--- src/main/java/leetcode/QueueReconstructionByHeight.java
+++ src/main/java/leetcode/QueueReconstructionByHeight.java
@@ -13,4 +13,13 @@ public class QueueReconstructionByHeight {
     }
     return result.toArray(new int[people.length][2]);
   }
+
+  public int[][] reconstructQueue2(int[][] people) {
+    Arrays.sort(people, (a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+    List<int[]> result = new ArrayList<>();
+    for (int[] person : people) {
+      result.add(person[1], person);
+    }
+    return result.toArray(new int[0][]);
+  }
 }
