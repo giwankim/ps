ceda97138b3044e8bb3df89ab9d790e9ff5d2e9e
Giwan Kim <giwan.kim@gmail.com>
2024-10-16 10:18:32 +0900

    refactor: LeetCode 242. Valid Anagram; sorting based solution
    https://leetcode.com/problems/valid-anagram/description/

--- src/main/java/leetcode/ValidAnagram.java
+++ src/main/java/leetcode/ValidAnagram.java
@@ -1,20 +1,13 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class ValidAnagram {
   public boolean isAnagram(String s, String t) {
-    Map<Character, Integer> sMap = new HashMap<>();
-    for (char c : s.toCharArray()) {
-      sMap.put(c, sMap.getOrDefault(c, 0) + 1);
-    }
-
-    Map<Character, Integer> tMap = new HashMap<>();
-    for (char c : t.toCharArray()) {
-      tMap.put(c, tMap.getOrDefault(c, 0) + 1);
-    }
-
-    return sMap.equals(tMap);
+    char[] sChars = s.toCharArray();
+    Arrays.sort(sChars);
+    char[] tChars = t.toCharArray();
+    Arrays.sort(tChars);
+    return Arrays.equals(sChars, tChars);
   }
 }
