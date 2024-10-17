2ba9acb394cdbfe1a4149ac6d416cd9f5a27f702
Giwan Kim <giwan.kim@gmail.com>
2024-10-17 12:36:41 +0900

    refactor: LeetCode 268. Missing Number; use Bit manipulation

--- src/main/java/leetcode/MissingNumber.java
+++ src/main/java/leetcode/MissingNumber.java
@@ -3,10 +3,9 @@ package leetcode;
 public class MissingNumber {
   public int missingNumber(int[] nums) {
     int n = nums.length;
-    int sum = ((n + 1) * n) / 2;
-    for (int num : nums) {
-      sum -= num;
+    for (int i = 0; i < nums.length; i++) {
+      n ^= i ^ nums[i];
     }
-    return sum;
+    return n;
   }
 }
