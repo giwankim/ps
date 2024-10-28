a18857493e68071426bf58894b5e86fa8fb10935
Giwan Kim <giwan.kim@gmail.com>
2024-10-29 07:33:39 +0900

    feat: LeetCode 98. Validate Binary Search Tree; iterative
    https://leetcode.com/problems/validate-binary-search-tree

--- src/main/java/leetcode/BinaryTreeInorderTraversal.java
+++ src/main/java/leetcode/BinaryTreeInorderTraversal.java
@@ -1,8 +1,7 @@
 package leetcode;
 
-import leetcode.support.TreeNode;
-
 import java.util.*;
+import leetcode.support.TreeNode;
 
 public class BinaryTreeInorderTraversal {
   public List<Integer> inorderTraversal(TreeNode root) {
