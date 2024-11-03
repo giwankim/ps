23c6b3899d49df380aa46829b0a00640612650e2
Giwan Kim <giwan.kim@gmail.com>
2024-11-03 17:47:54 +0900

    feat: LeetCode 647. Palindromic Substrings; DP O(n^2)
    https://leetcode.com/problems/palindromic-substrings

--- src/main/java/leetcode/PalindromicSubstrings.java
+++ src/main/java/leetcode/PalindromicSubstrings.java
@@ -3,12 +3,34 @@ package leetcode;
 public class PalindromicSubstrings {
   public int countSubstrings(String s) {
     int cnt = 0;
-    for (int i = 0; i < s.length(); i++) {
-      for (int j = i; j < s.length(); j++) {
-        String t = s.substring(i, j + 1);
-        String reversed = new StringBuilder(t).reverse().toString();
-        if (t.equals(reversed)) {
+    int n = s.length();
+    boolean[][] isPalindrome = new boolean[n][n];
+
+    for (int i = 0; i < n; i++) {
+      isPalindrome[i][i] = true;
+      cnt += 1;
+    }
+
+    for (int i = 0; i + 1 < n; i++) {
+      if (s.charAt(i) == s.charAt(i + 1)) {
+        isPalindrome[i][i + 1] = true;
+        cnt += 1;
+      } else {
+        isPalindrome[i][i + 1] = false;
+      }
+    }
+
+    for (int len = 3; len <= n; len++) {
+      for (int i = 0; i < n; i++) {
+        int j = i + len - 1;
+        if (j >= n) {
+          continue;
+        }
+        if (s.charAt(i) == s.charAt(j) && isPalindrome[i + 1][j - 1]) {
+          isPalindrome[i][j] = true;
           cnt += 1;
+        } else {
+          isPalindrome[i][j] = false;
         }
       }
     }
