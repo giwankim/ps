012af59cc349d7d3fd3acf7285de741af5fab316
Giwan Kim <giwan.kim@gmail.com>
2025-01-10 12:08:23 +0900

    refactor: LeetCode - 45. Jump Game II
    link: https://leetcode.com/problems/jump-game-ii

--- src/main/java/leetcode/JumpGameII.java
+++ src/main/java/leetcode/JumpGameII.java
@@ -1,20 +1,17 @@
 package leetcode;
 
-import java.util.Arrays;
-
 public class JumpGameII {
   public int jump(int[] nums) {
-    int[] a = new int[nums.length];
-    Arrays.fill(a, Integer.MAX_VALUE);
-    a[0] = 0;
-    for (int i = 0; i < a.length; i++) {
-      for (int j = 1; j <= nums[i]; j++) {
-        if (i + j >= nums.length) {
-          continue;
-        }
-        a[i + j] = Math.min(a[i + j], a[i] + 1);
+    int result = 0;
+    int farthest = 0;
+    int end = 0;
+    for (int i = 0; i + 1 < nums.length; i++) {
+      farthest = Math.max(farthest, i + nums[i]);
+      if (i == end) {
+        result += 1;
+        end = farthest;
       }
     }
-    return a[nums.length - 1];
+    return result;
   }
 }
---
+++ src/main/kotlin/leetcode/JumpGameIIKt.kt
@@ -0,0 +1,17 @@
+package leetcode
+
+class JumpGameIIKt {
+    fun jump(nums: IntArray): Int {
+        var result = 0
+        var farthest = 0
+        var end = 0
+        for (i in 0 until nums.size - 1) {
+            farthest = maxOf(farthest, i + nums[i])
+            if (i == end) {
+                result += 1
+                end = farthest
+            }
+        }
+        return result
+    }
+}
