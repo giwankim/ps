6db5dad04c58b0f5bd1195f7d7d9dfe6e1e970db
Giwan Kim <giwan.kim@gmail.com>
2025-01-11 21:07:26 +0900

    feat: LeetCode - 41. First Missing Positive in Kotlin
    link: https://leetcode.com/problems/first-missing-positive

---
+++ src/main/kotlin/leetcode/FirstMissingPositiveKt.kt
@@ -0,0 +1,26 @@
+package leetcode
+
+class FirstMissingPositiveKt {
+    fun firstMissingPositive(nums: IntArray): Int {
+        for (i in nums.indices) {
+            while (nums[i] in 1..nums.size && nums[i] != nums[nums[i] - 1]) {
+                nums.swap(i, nums[i] - 1)
+            }
+        }
+        for (i in nums.indices) {
+            if (nums[i] != i + 1) {
+                return i + 1
+            }
+        }
+        return nums.size + 1
+    }
+
+    fun IntArray.swap(
+        i: Int,
+        j: Int,
+    ) {
+        val temp = this[i]
+        this[i] = this[j]
+        this[j] = temp
+    }
+}
---
+++ src/test/kotlin/leetcode/FirstMissingPositiveKtTest.kt
@@ -0,0 +1,29 @@
+package leetcode
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.WithDataTestName
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+
+class FirstMissingPositiveKtTest :
+    FunSpec(
+        {
+            context("first missing positive") {
+                withData(
+                    FirstMissingPositiveTestCase(intArrayOf(1, 2, 0), 3),
+                    FirstMissingPositiveTestCase(intArrayOf(3, 4, -1, 1), 2),
+                    FirstMissingPositiveTestCase(intArrayOf(7, 8, 9, 11, 12), 1),
+                ) { (nums, expected) ->
+                    val sut = FirstMissingPositiveKt()
+                    sut.firstMissingPositive(nums) shouldBe expected
+                }
+            }
+        },
+    )
+
+data class FirstMissingPositiveTestCase(
+    val nums: IntArray,
+    val expected: Int,
+) : WithDataTestName {
+    override fun dataTestName(): String = "nums = ${nums.contentToString()}, expected = $expected"
+}
