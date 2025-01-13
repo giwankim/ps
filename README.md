5c63decd3d7e5a1cca9cdd30a1607e8ac2734f3f
Giwan Kim <giwan.kim@gmail.com>
2025-01-13 13:07:25 +0900

    feat: LeetCode - 102. Binary Tree Level Order Traversal
    link: https://leetcode.com/problems/binary-tree-level-order-traversal

--- README.md
+++ README.md
@@ -95,6 +95,7 @@
 * [886. Possible Bipartition](https://leetcode.com/problems/possible-bipartition)
 * [45. Jump Game II](https://leetcode.com/problems/jump-game-ii)
 * [41. First Missing Positive](https://leetcode.com/problems/first-missing-positive)
+* [102. Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/description/)
 
 ## [Programmers](https://programmers.co.kr/)
 
---
+++ src/test/kotlin/leetcode/BinaryTreeLevelOrderTraversalKtTest.kt
@@ -0,0 +1,39 @@
+package leetcode
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.WithDataTestName
+import io.kotest.datatest.withData
+import io.kotest.matchers.shouldBe
+import leetcode.support.TreeNode
+
+class BinaryTreeLevelOrderTraversalKtTest :
+    FunSpec(
+        {
+            context("binary tree level order traversal") {
+                withData(
+                    BinaryTreeLevelOrderTraversalTestCase(
+                        TreeNode.from(3, 9, 20, null, null, 15, 7),
+                        listOf(listOf(3), listOf(9, 20), listOf(15, 7)),
+                    ),
+                    BinaryTreeLevelOrderTraversalTestCase(
+                        TreeNode.from(1),
+                        listOf(listOf(1)),
+                    ),
+                    BinaryTreeLevelOrderTraversalTestCase(
+                        TreeNode.from(),
+                        emptyList(),
+                    ),
+                ) { (root, expected) ->
+                    val sut = BinaryTreeLevelOrderTraversalKt()
+                    sut.levelOrder(root) shouldBe expected
+                }
+            }
+        },
+    )
+
+data class BinaryTreeLevelOrderTraversalTestCase(
+    val root: TreeNode?,
+    val expected: List<List<Int>>,
+) : WithDataTestName {
+    override fun dataTestName(): String = "root=${root?.prettyPrint()}, expected=$expected"
+}
