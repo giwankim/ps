ca1dea5ebc78ba0145536129bbaba53a2344c2b4
Giwan Kim <giwan.kim@gmail.com>
2025-02-26 11:58:22 +0900

    feat: LeetCode - 13. Roman to Integer
    https://leetcode.com/problems/roman-to-integer

---
+++ src/test/java/leetcode/RomanToIntegerTest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class RomanToIntegerTest {
+  @ParameterizedTest
+  @MethodSource
+  void romanToInt(String s, int expected) {
+    var sut = new RomanToInteger();
+    int actual = sut.romanToInt(s);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> romanToInt() {
+    return Stream.of(
+        Arguments.of("III", 3), Arguments.of("LVIII", 58), Arguments.of("MCMXCIV", 1994));
+  }
+}
