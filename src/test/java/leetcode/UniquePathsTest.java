270aad86de152ac372cd80534808ce8171dd400b
Giwan Kim <giwan.kim@gmail.com>
2025-02-26 13:44:47 +0900

    feat: LeetCode - 36. Valid Sudoku
    link: https://leetcode.com/problems/valid-sudoku

--- src/test/java/leetcode/UniquePathsTest.java
+++ src/test/java/leetcode/UniquePathsTest.java
@@ -1,12 +1,11 @@
 package leetcode;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class UniquePathsTest {
   @ParameterizedTest
