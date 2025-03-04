1c39ee6a932458734e9c28b66421ed0df4dac23a
Giwan Kim <giwan.kim@gmail.com>
2025-03-04 11:54:22 +0900

    feat: LeetCode - 71. Simplify Path (Java)
    link: https://leetcode.com/problems/simplify-path

---
+++ src/test/java/leetcode/SimplifyPathTest.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimplifyPathTest {
+  private SimplifyPath sut;
+
+  @BeforeEach
+  void setUp() {
+    sut = new SimplifyPath();
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void simplifyPath(String path, String expected) {
+    String simplifiedPath = sut.simplifyPath(path);
+    assertThat(simplifiedPath).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> simplifyPath() {
+    return Stream.of(
+        Arguments.of("/home/", "/home"),
+        Arguments.of("/home//foo", "/home/foo"),
+        Arguments.of("/home/user/Documents/../Pictures", "/home/user/Pictures"),
+        Arguments.of("/../", "/"),
+        Arguments.of("/a/./b/../../c/", "/c"));
+  }
+}
