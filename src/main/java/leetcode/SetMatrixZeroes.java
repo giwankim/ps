90485ed60d47a9d9634d6250330524661ce27e09
Giwan Kim <giwan.kim@gmail.com>
2025-03-08 10:00:35 +0900

    feat: LeetCode - 73. Set Matrix Zeroes
    link: https://leetcode.com/problems/set-matrix-zeroes

---
+++ src/main/java/leetcode/SetMatrixZeroes.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetMatrixZeroes {
+  public void setZeroes(int[][] matrix) {
+    Set<Integer> rows = new HashSet<>();
+    Set<Integer> cols = new HashSet<>();
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] == 0) {
+          rows.add(i);
+          cols.add(j);
+        }
+      }
+    }
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (rows.contains(i) || cols.contains(j)) {
+          matrix[i][j] = 0;
+        }
+      }
+    }
+  }
+}
