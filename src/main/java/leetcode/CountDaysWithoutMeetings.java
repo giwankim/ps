abfc475afeb080eff922e19f2728f580c67abbf8
Giwan Kim <giwan.kim@gmail.com>
2025-09-10 12:30:19 +0900

    refactor: simplify CountDaysWithoutMeetings and clean up unused code
    - Remove unnecessary variables and redundant logic for merging intervals.
    - Streamline algorithm for counting days without meetings.
    - Minor formatting adjustments in test cases.

--- src/main/java/leetcode/CountDaysWithoutMeetings.java
+++ src/main/java/leetcode/CountDaysWithoutMeetings.java
@@ -1,52 +1,28 @@
 package leetcode;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class CountDaysWithoutMeetings {
 
   public int countDays(int days, int[][] meetings) {
     // sort the meetings by start time then end time
-    /*
-    Arrays.sort(
-        meetings,
-        Comparator.comparingInt((int[] meeting) -> meeting[0])
-            .thenComparingInt(meeting -> meeting[1]));
-     */
-    Arrays.sort(
-        meetings,
-        (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+    Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
 
-    // merge overlapping meetings
-    List<int[]> mergedMeetings = new ArrayList<>();
+    // add the days until the first meeting
+    int result = meetings[0][0] - 1;
 
-    int start = meetings[0][0];
-    int end = meetings[0][1];
-
-    for (int i = 1; i < meetings.length; i++) {
-      if (meetings[i][0] <= end) {
-        // merge the meeting times
-        end = Math.max(end, meetings[i][1]);
+    for (int i = 0; i + 1 < meetings.length; i++) {
+      if (meetings[i][1] >= meetings[i + 1][0]) { // meetings overlap
+        // merge meetings
+        meetings[i + 1][1] = Math.max(meetings[i][1], meetings[i + 1][1]);
       } else {
-        // meeting times do not overlap
-        mergedMeetings.add(new int[] {start, end});
-        start = meetings[i][0];
-        end = meetings[i][1];
+        result += meetings[i + 1][0] - meetings[i][1] - 1;
       }
     }
-    mergedMeetings.add(new int[] {start, end});
 
-    // find complementary days to mergedMeetings interval
-    int result = mergedMeetings.getFirst()[0] - 1;
-    for (int i = 0; i + 1 < mergedMeetings.size(); i++) {
-      if (mergedMeetings.get(i + 1)[0] <= days) {
-        result += mergedMeetings.get(i + 1)[0] - mergedMeetings.get(i)[1] - 1;
-      }
-    }
-    if (mergedMeetings.getLast()[1] < days) {
-      result += days - mergedMeetings.getLast()[1];
-    }
+    // add days from the end of last meeting
+    result += days - meetings[meetings.length - 1][1];
+
     return result;
   }
 }
