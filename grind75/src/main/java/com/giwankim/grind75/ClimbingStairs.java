f7268053d4a31f5c625bacc88f1b5ecd30b1158e
Giwan Kim <giwan.kim@gmail.com>
2025-10-03 11:40:43 +0900

    feat(grind75): implement Climbing Stairs solution with tests
    - Add `ClimbingStairs` class to calculate the number of distinct ways to climb stairs using dynamic programming.
    - Write `ParameterizedTest` cases in `ClimbingStairsTest` to validate scenarios with varying step counts.

---
+++ grind75/src/main/java/com/giwankim/grind75/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package com.giwankim.grind75;
+
+public class ClimbingStairs {
+
+  public int climbStairs(int n) {
+    int[] dp = new int[n + 1];
+    dp[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+      dp[i] = dp[i - 1];
+      if (i >= 2) {
+        dp[i] += dp[i - 2];
+      }
+    }
+    return dp[n];
+  }
+}
---
+++ grind75/src/test/java/com/giwankim/grind75/ClimbingStairsTest.java
@@ -0,0 +1,25 @@
+package com.giwankim.grind75;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ClimbingStairsTest {
+
+  @ParameterizedTest
+  @MethodSource
+  void climbStairs(int n, int expected) {
+    int actual = new ClimbingStairs().climbStairs(n);
+    assertThat(actual).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> climbStairs() {
+    return Stream.of(
+        Arguments.of(2, 2),
+        Arguments.of(3, 3)
+    );
+  }
+}
