c03ed5c13046755841102c97cb8d2d53c0add03a
Giwan Kim <giwan.kim@gmail.com>
2025-11-29 21:35:20 +0900

    feat(grind75): implement Container With Most Water solution with tests
    - Add `ContainerWithMostWater` class to calculate the maximum area of water a container can hold.
    - Write `ParameterizedTest` cases in `ContainerWithMostWaterTest` to validate scenarios with different height arrays.

---
+++ grind75/src/main/java/com/giwankim/grind75/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package com.giwankim.grind75;
+
+public class ContainerWithMostWater {
+  public int maxArea(int[] height) {
+    int result = 0;
+
+    int left = 0;
+    int right = height.length - 1;
+
+    while (left < right) {
+      int width = right - left;
+      if (height[left] < height[right]) {
+        result = Math.max(result, width * height[left]);
+        left += 1;
+      } else {
+        result = Math.max(result, width * height[right]);
+        right -= 1;
+      }
+    }
+
+    return result;
+  }
+}
