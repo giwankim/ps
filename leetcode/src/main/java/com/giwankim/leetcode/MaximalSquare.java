0e56daf97241f97a7a0a712f4f470010c6cf1cee
Giwan Kim <giwan.kim@gmail.com>
2026-01-25 07:33:03 +0900

    feat(leetcode): add MaximalSquare DP solution
    Implement dynamic programming approach to find the largest square
    containing only 1s in a binary matrix.

--- leetcode/src/main/java/com/giwankim/leetcode/MaximalSquare.java
+++ leetcode/src/main/java/com/giwankim/leetcode/MaximalSquare.java
@@ -1,4 +1,35 @@
 package com.giwankim.leetcode;
 
 public class MaximalSquare {
+
+  public int maximalSquare(char[][] matrix) {
+    int result = 0;
+    int m = matrix.length;
+    int n = matrix[0].length;
+
+    int[][] a = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      a[i][0] = matrix[i][0] == '1' ? 1 : 0;
+    }
+    for (int j = 0; j < n; j++) {
+      a[0][j] = matrix[0][j] == '1' ? 1 : 0;
+    }
+
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        if (matrix[i][j] == '0') {
+          a[i][j] = 0;
+        } else {
+          a[i][j] = 1 + Math.min(Math.min(a[i - 1][j], a[i][j - 1]), a[i - 1][j - 1]);
+        }
+      }
+    }
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        result = Math.max(result, a[i][j] * a[i][j]);
+      }
+    }
+    return result;
+  }
 }
--- leetcode/src/test/java/com/giwankim/leetcode/MaximalSquareTest.java
+++ leetcode/src/test/java/com/giwankim/leetcode/MaximalSquareTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.giwankim.leetcode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class MaximalSquareTest {
-  
+
+  MaximalSquare subject = new MaximalSquare();
+
+  @ParameterizedTest
+  @MethodSource
+  void maximalSquare(char[][] matrix, int expected) {
+    assertThat(subject.maximalSquare(matrix)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> maximalSquare() {
+    return Stream.of(
+        Arguments.of(new char[][] {{'0'}}, 0),
+        Arguments.of(new char[][] {{'1'}}, 1),
+        Arguments.of(new char[][] {{'0', '1'}, {'1', '0'}}, 1),
+        Arguments.of(
+            new char[][] {
+              {'1', '0', '1', '0', '0'},
+              {'1', '0', '1', '1', '1'},
+              {'1', '1', '1', '1', '1'},
+              {'1', '0', '0', '1', '0'}
+            },
+            4));
+  }
 }
