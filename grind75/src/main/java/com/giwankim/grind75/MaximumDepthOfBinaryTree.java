205b9ead374c9f2b67b5d6bfdb80cabbdcaf33e3
Giwan Kim <giwan.kim@gmail.com>
2026-01-28 12:04:38 +0900

    refactor(grind75): rename MaximumDepthBinaryTree to MaximumDepthOfBinaryTree
    Align class name with LeetCode problem title.

--- grind75/src/main/java/com/giwankim/grind75/MaximumDepthOfBinaryTree.java
+++ grind75/src/main/java/com/giwankim/grind75/MaximumDepthOfBinaryTree.java
@@ -8,7 +8,7 @@ import com.giwankim.grind75.support.TreeNode;
  * <p>Maximum depth is the number of nodes along the longest path from the root node down to the
  * farthest leaf node.
  */
-public class MaximumDepthBinaryTree {
+public class MaximumDepthOfBinaryTree {
 
   public int maxDepth(TreeNode root) {
     if (root == null) {
--- grind75/src/test/java/com/giwankim/grind75/MaximumDepthOfBinaryTreeTest.java
+++ grind75/src/test/java/com/giwankim/grind75/MaximumDepthOfBinaryTreeTest.java
@@ -5,17 +5,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.giwankim.grind75.support.TreeNode;
 import org.junit.jupiter.api.Test;
 
-class MaximumDepthBinaryTreeTest {
+class MaximumDepthOfBinaryTreeTest {
 
-  MaximumDepthBinaryTree maximumDepthBinaryTree = new MaximumDepthBinaryTree();
+  MaximumDepthOfBinaryTree sut = new MaximumDepthOfBinaryTree();
 
   @Test
   void maxDepth() {
-    assertThat(maximumDepthBinaryTree.maxDepth(null)).isZero();
-    assertThat(maximumDepthBinaryTree.maxDepth(new TreeNode(1, null, new TreeNode(2))))
-        .isEqualTo(2);
+    assertThat(sut.maxDepth(null)).isZero();
+    assertThat(sut.maxDepth(new TreeNode(1, null, new TreeNode(2)))).isEqualTo(2);
     assertThat(
-            maximumDepthBinaryTree.maxDepth(
+            sut.maxDepth(
                 new TreeNode(
                     3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))))
         .isEqualTo(3);
