1060288734bcb7ebb16b5e2a9c12c53f18a00d68
Giwan Kim <giwan.kim@gmail.com>
2026-02-04 13:54:12 +0900

    refactor(leetcode): optimize Rotate Array to O(1) space

--- leetcode/src/main/java/com/giwankim/leetcode/RotateArray.java
+++ leetcode/src/main/java/com/giwankim/leetcode/RotateArray.java
@@ -1,16 +1,21 @@
 package com.giwankim.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class RotateArray {
   public void rotate(int[] nums, int k) {
-    List<Integer> list = new ArrayList<>(Arrays.stream(nums).boxed().toList());
-    Collections.rotate(list, k);
-    for (int i = 0; i < nums.length; i++) {
-      nums[i] = list.get(i);
+    // Time complexity: O(n), Space complexity: O(1)
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - k - 1);
+    reverse(nums, nums.length - k, nums.length - 1);
+    reverse(nums, 0, nums.length - 1);
+  }
+
+  private void reverse(int[] nums, int left, int right) {
+    while (left < right) {
+      int tmp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = tmp;
+      left += 1;
+      right -= 1;
     }
   }
 }
