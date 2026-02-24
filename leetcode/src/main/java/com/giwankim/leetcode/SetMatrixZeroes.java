e777593cb2cfd105c3666e254f121692fe2fd2d2
Giwan Kim <giwan.kim@gmail.com>
2026-02-24 14:40:52 +0900

    feat(leetcode): add Set Matrix Zeroes solution

--- leetcode/src/main/java/com/giwankim/leetcode/SetMatrixZeroes.java
+++ leetcode/src/main/java/com/giwankim/leetcode/SetMatrixZeroes.java
@@ -1,27 +1,47 @@
 package com.giwankim.leetcode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class SetMatrixZeroes {
   public void setZeroes(int[][] matrix) {
-    Set<Integer> rows = new HashSet<>();
-    Set<Integer> cols = new HashSet<>();
-
+    // Time complexity: O(mn), Space complexity: O(1)
+    boolean isFirstRow = false;
+    for (int j = 0; j < matrix[0].length; j++) {
+      if (matrix[0][j] == 0) {
+        isFirstRow = true;
+      }
+    }
+    boolean isFirstCol = false;
     for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][0] == 0) {
+        isFirstCol = true;
+      }
+    }
+
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[i].length; j++) {
         if (matrix[i][j] == 0) {
-          rows.add(i);
-          cols.add(j);
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
         }
       }
     }
 
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix[i].length; j++) {
-        if (rows.contains(i) || cols.contains(j)) {
+    for (int i = 1; i < matrix.length; i++) {
+      for (int j = 1; j < matrix[i].length; j++) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
           matrix[i][j] = 0;
         }
+      }
+    }
+
+    if (isFirstRow) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        matrix[0][j] = 0;
+      }
+    }
+
+    if (isFirstCol) {
+      for (int i = 0; i < matrix.length; i++) {
+        matrix[i][0] = 0;
       }
     }
   }
--- leetcode/src/test/java/com/giwankim/leetcode/SetMatrixZeroesTest.java
+++ leetcode/src/test/java/com/giwankim/leetcode/SetMatrixZeroesTest.java
@@ -3,17 +3,52 @@ package com.giwankim.leetcode;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class SetMatrixZeroesTest {
-  SetMatrixZeroes sut;
+  SetMatrixZeroes sut = new SetMatrixZeroes();
 
-  @BeforeEach
-  void setUp() {
-    sut = new SetMatrixZeroes();
+  @Test
+  void neitherFirstRowNorFirstColumnContainsZero() {
+    int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int[][] expected = {{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+    sut.setZeroes(matrix);
+    assertThat(matrix).isDeepEqualTo(expected);
+  }
+
+  @Test
+  void firstRowContainsZeroFirstColumnNoZero() {
+    int[][] matrix = {{1, 2, 0}, {3, 4, 5}, {6, 7, 9}};
+    int[][] expected = {{0, 0, 0}, {3, 4, 0}, {6, 7, 0}};
+    sut.setZeroes(matrix);
+    assertThat(matrix).isDeepEqualTo(expected);
+  }
+
+  @Test
+  void firstRowNoZeroFirstColumnContainsZero() {
+    int[][] matrix = {{1, 2, 3}, {0, 4, 5}, {6, 7, 8}};
+    int[][] expected = {{0, 2, 3}, {0, 0, 0}, {0, 7, 8}};
+    sut.setZeroes(matrix);
+    assertThat(matrix).isDeepEqualTo(expected);
+  }
+
+  @Test
+  void firstRowContainsZeroFirstColumnContainsZero() {
+    int[][] matrix = {{1, 0, 2}, {0, 3, 4}, {5, 6, 7}};
+    int[][] expected = {{0, 0, 0}, {0, 0, 0}, {0, 0, 7}};
+    sut.setZeroes(matrix);
+    assertThat(matrix).isDeepEqualTo(expected);
+  }
+
+  @Test
+  void leftRightCornerZero() {
+    int[][] matrix = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+    int[][] expected = {{0, 0, 0}, {0, 4, 5}, {0, 7, 8}};
+    sut.setZeroes(matrix);
+    assertThat(matrix).isDeepEqualTo(expected);
   }
 
   @ParameterizedTest
