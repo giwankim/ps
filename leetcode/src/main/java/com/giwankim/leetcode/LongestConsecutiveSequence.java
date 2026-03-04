42e1d0765474793fe1aa2f10a08d642b3014c71c
Giwan Kim <giwan.kim@gmail.com>
2026-03-04 11:18:10 +0900

    feat(leetcode): add Longest Consecutive Sequence solution and tests

--- leetcode/src/main/java/com/giwankim/leetcode/LongestConsecutiveSequence.java
+++ leetcode/src/main/java/com/giwankim/leetcode/LongestConsecutiveSequence.java
@@ -5,20 +5,21 @@ import java.util.Set;
 
 public class LongestConsecutiveSequence {
   public int longestConsecutive(int[] nums) {
+    // Time complexity: O(n), Space complexity: O(n)
     Set<Integer> set = new HashSet<>();
     for (int num : nums) {
       set.add(num);
     }
     int result = 0;
-    for (int x : nums) {
-      if (set.contains(x - 1)) {
+    for (int num : set) {
+      if (set.contains(num - 1)) {
         continue;
       }
-      int y = x;
-      while (set.contains(y + 1)) {
-        y += 1;
+      int length = 1;
+      while (set.contains(num + length)) {
+        length += 1;
       }
-      result = Math.max(result, y - x + 1);
+      result = Math.max(result, length);
     }
     return result;
   }
--- leetcode/src/test/java/com/giwankim/leetcode/LongestConsecutiveSequenceTest.java
+++ leetcode/src/test/java/com/giwankim/leetcode/LongestConsecutiveSequenceTest.java
@@ -8,17 +8,21 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class LongestConsecutiveSequenceTest {
+  LongestConsecutiveSequence sut = new LongestConsecutiveSequence();
 
   @ParameterizedTest
   @MethodSource
   void longestConsecutive(int[] nums, int expected) {
-    int actual = new LongestConsecutiveSequence().longestConsecutive(nums);
+    int actual = sut.longestConsecutive(nums);
     assertThat(actual).isEqualTo(expected);
   }
 
   private static Stream<Arguments> longestConsecutive() {
     return Stream.of(
+        Arguments.of(new int[] {}, 0),
+        Arguments.of(new int[] {0}, 1),
         Arguments.of(new int[] {100, 4, 200, 1, 3, 2}, 4),
-        Arguments.of(new int[] {0, 3, 7, 2, 5, 8, 4, 6, 0, 1}, 9));
+        Arguments.of(new int[] {0, 3, 7, 2, 5, 8, 4, 6, 0, 1}, 9),
+        Arguments.of(new int[] {1, 2, 0, 1}, 3));
   }
 }
