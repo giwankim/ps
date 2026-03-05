cf3917e915904e24bb25f03967e9b174d897f2c8
Giwan Kim <giwan.kim@gmail.com>
2026-03-05 12:06:14 +0900

    style(leetcode): clean up NumberOf1Bits implementation and test setup
    - simplify bit-count expression formatting in hammingWeight
    - reuse a shared SUT instance in NumberOf1BitsTest
    - normalize CsvSource formatting

--- leetcode/src/main/java/com/giwankim/leetcode/NumberOf1Bits.java
+++ leetcode/src/main/java/com/giwankim/leetcode/NumberOf1Bits.java
@@ -4,7 +4,7 @@ public class NumberOf1Bits {
   public int hammingWeight(int n) {
     int result = 0;
     while (n != 0) {
-      result += (n & 1);
+      result += n & 1;
       n >>= 1;
     }
     return result;
--- leetcode/src/test/java/com/giwankim/leetcode/NumberOf1BitsTest.java
+++ leetcode/src/test/java/com/giwankim/leetcode/NumberOf1BitsTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class NumberOf1BitsTest {
+  NumberOf1Bits sut = new NumberOf1Bits();
 
   @ParameterizedTest
-  @CsvSource({"11, 3", "128, 1", "2147483645, 30"})
+  @CsvSource({"11,3", "128,1", "2147483645,30"})
   void hammingWeight(int n, int expected) {
-    int actual = new NumberOf1Bits().hammingWeight(n);
+    int actual = sut.hammingWeight(n);
     assertThat(actual).isEqualTo(expected);
   }
 }
