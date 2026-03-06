310fa16cb716739550e0131fff7b080254f8c587
Giwan Kim <giwan.kim@gmail.com>
2026-03-06 11:09:22 +0900

    Implement LinkedListCycle and simplify cycle tests

--- leetcode/src/main/java/com/giwankim/leetcode/LinkedListCycle.java
+++ leetcode/src/main/java/com/giwankim/leetcode/LinkedListCycle.java
@@ -4,6 +4,7 @@ import com.giwankim.leetcode.support.ListNode;
 
 public class LinkedListCycle {
   public boolean hasCycle(ListNode head) {
+    // Time Complexity: O(n), Space Complexity: O(1)
     ListNode slow = head;
     ListNode fast = head;
     while (fast != null && fast.next != null) {
--- leetcode/src/test/java/com/giwankim/leetcode/LinkedListCycleTest.java
+++ leetcode/src/test/java/com/giwankim/leetcode/LinkedListCycleTest.java
@@ -5,28 +5,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.giwankim.leetcode.support.ListNode;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class LinkedListCycleTest {
+  private final LinkedListCycle sut = new LinkedListCycle();
 
   @ParameterizedTest
   @MethodSource
-  void hasCycle(ListNode head, boolean expected) {
-    boolean actual = new LinkedListCycle().hasCycle(head);
-    assertThat(actual).isEqualTo(expected);
+  void noCycle(ListNode head) {
+    assertThat(sut.hasCycle(head)).isFalse();
   }
 
-  private static Stream<Arguments> hasCycle() {
-    ListNode head1 = ListNode.of(3, 2, 0, -4);
-    head1.next.next.next.next = head1.next;
+  private static Stream<ListNode> noCycle() {
+    return Stream.of(null, ListNode.of(1), ListNode.of(1, 2), ListNode.of(1, 2, 3, 4, 5));
+  }
 
-    ListNode head2 = ListNode.of(1, 2);
-    head2.next.next = head2;
+  @ParameterizedTest
+  @MethodSource
+  void hasCycle(ListNode head) {
+    assertThat(sut.hasCycle(head)).isTrue();
+  }
 
-    ListNode head3 = ListNode.of(1);
-
+  private static Stream<ListNode> hasCycle() {
     return Stream.of(
-        Arguments.of(head1, true), Arguments.of(head2, true), Arguments.of(head3, false));
+        listWithTailConnectedTo(new int[] {1}, 0),
+        listWithTailConnectedTo(new int[] {1, 2}, 0),
+        listWithTailConnectedTo(new int[] {3, 2, 0, -4}, 1));
+  }
+
+  private static ListNode listWithTailConnectedTo(int[] values, int pos) {
+    if (values.length == 0) {
+      return null;
+    }
+
+    ListNode head = ListNode.of(values);
+    if (pos < 0) {
+      return head;
+    }
+
+    ListNode target = null;
+    ListNode tail = head;
+    int index = 0;
+    for (ListNode current = head; current != null; current = current.next) {
+      if (index == pos) {
+        target = current;
+      }
+      tail = current;
+      index++;
+    }
+
+    if (target == null) {
+      throw new IllegalArgumentException("pos is out of range: " + pos);
+    }
+
+    tail.next = target;
+    return head;
   }
 }
