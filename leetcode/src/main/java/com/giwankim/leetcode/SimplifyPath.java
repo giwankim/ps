571b41c663e0eb9124e742b84179189b8aee43d5
Giwan Kim <giwan.kim@gmail.com>
2026-03-10 23:21:05 +0900

    refactor(leetcode): streamline simplify path solution

--- leetcode/src/main/java/com/giwankim/leetcode/SimplifyPath.java
+++ leetcode/src/main/java/com/giwankim/leetcode/SimplifyPath.java
@@ -1,36 +1,20 @@
 package com.giwankim.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class SimplifyPath {
   public String simplifyPath(String path) {
-    List<String> list = new ArrayList<>();
-    String[] components = path.split("/");
-
-    for (String component : components) {
-      if (component.isBlank() || ".".equals(component)) {
-        continue;
-      }
-
-      if ("..".equals(component)) {
-        if (!list.isEmpty()) {
-          list.removeLast();
-        }
-      } else {
-        list.add(component);
+    // Time complexity: O(n), Space complexity: O(n)
+    String[] tokens = path.split("/");
+    Deque<String> deque = new ArrayDeque<>();
+    for (String token : tokens) {
+      switch (token) {
+        case "", "." -> {}
+        case ".." -> deque.pollLast();
+        default -> deque.addLast(token);
       }
     }
-
-    StringBuilder sb = new StringBuilder();
-    for (String component : list) {
-      sb.append("/").append(component);
-    }
-
-    if (sb.isEmpty()) {
-      return "/";
-    } else {
-      return sb.toString();
-    }
+    return "/" + String.join("/", deque);
   }
 }
--- leetcode/src/test/java/com/giwankim/leetcode/SimplifyPathTest.java
+++ leetcode/src/test/java/com/giwankim/leetcode/SimplifyPathTest.java
@@ -2,33 +2,23 @@ package com.giwankim.leetcode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.stream.Stream;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class SimplifyPathTest {
-  private SimplifyPath sut;
-
-  @BeforeEach
-  void setUp() {
-    sut = new SimplifyPath();
-  }
+  SimplifyPath sut = new SimplifyPath();
 
   @ParameterizedTest
-  @MethodSource
+  @CsvSource(
+      value = {
+        "/home/:/home",
+        "/home//foo:/home/foo",
+        "/home/user/Documents/../Pictures:/home/user/Pictures",
+        "/../:/",
+        "/a/./b/../../c/:/c"
+      },
+      delimiter = ':')
   void simplifyPath(String path, String expected) {
-    String simplifiedPath = sut.simplifyPath(path);
-    assertThat(simplifiedPath).isEqualTo(expected);
-  }
-
-  static Stream<Arguments> simplifyPath() {
-    return Stream.of(
-        Arguments.of("/home/", "/home"),
-        Arguments.of("/home//foo", "/home/foo"),
-        Arguments.of("/home/user/Documents/../Pictures", "/home/user/Pictures"),
-        Arguments.of("/../", "/"),
-        Arguments.of("/a/./b/../../c/", "/c"));
+    assertThat(sut.simplifyPath(path)).isEqualTo(expected);
   }
 }
