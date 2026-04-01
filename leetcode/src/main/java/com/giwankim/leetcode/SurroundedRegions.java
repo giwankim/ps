1d28a0dbe66abcb817f356caefa57034461dc985
Giwan Kim <giwan.kim@gmail.com>
2026-04-01 10:05:15 +0900

    refactor(leetcode): replace BFS with DFS and in-place markers for Surrounded Regions
    - Replace BFS queue + visited array with recursive DFS using '#' sentinel
    - Use in-place board marking to eliminate extra boolean[][] allocation
    - Rename variables to descriptive rows/cols for readability

--- leetcode/src/main/java/com/giwankim/leetcode/SurroundedRegions.java
+++ leetcode/src/main/java/com/giwankim/leetcode/SurroundedRegions.java
@@ -1,57 +1,42 @@
 package com.giwankim.leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class SurroundedRegions {
   private static final int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
   public void solve(char[][] board) {
-    // Time complexity: O(m * n), Space complexity: O(m * n)
-    int n = board.length;
-    int m = board[0].length;
+    // Time complexity: O(cols * rows), Space complexity: O(1)
+    int rows = board.length;
+    int cols = board[0].length;
 
-    Queue<int[]> queue = new ArrayDeque<>();
-    boolean[][] visited = new boolean[n][m];
-    for (int i = 0; i < n; i++) {
-      if (board[i][0] == 'O') {
-        queue.offer(new int[] {i, 0});
-        visited[i][0] = true;
-      }
-      if (board[i][m - 1] == 'O') {
-        queue.offer(new int[] {i, m - 1});
-        visited[i][m - 1] = true;
-      }
+    // Mark the 'O' cells connected to the borders
+    for (int i = 0; i < rows; i++) {
+      dfs(board, i, 0);
+      dfs(board, i, cols - 1);
     }
-    for (int j = 1; j + 1 < m; j++) {
-      if (board[0][j] == 'O') {
-        queue.offer(new int[] {0, j});
-        visited[0][j] = true;
-      }
-      if (board[n - 1][j] == 'O') {
-        queue.offer(new int[] {n - 1, j});
-        visited[n - 1][j] = true;
-      }
-    }
-    while (!queue.isEmpty()) {
-      int[] cell = queue.poll();
-      for (int[] dir : DIRECTIONS) {
-        int nx = cell[0] + dir[0];
-        int ny = cell[1] + dir[1];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || board[nx][ny] != 'O') {
-          continue;
-        }
-        queue.offer(new int[] {nx, ny});
-        visited[nx][ny] = true;
-      }
+    for (int j = 1; j + 1 < cols; j++) {
+      dfs(board, 0, j);
+      dfs(board, rows - 1, j);
     }
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (board[i][j] == 'O' && !visited[i][j]) {
+    // Replace 'O' cells not connected to the border with 'X'
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (board[i][j] == '#') {
+          board[i][j] = 'O';
+        } else if (board[i][j] == 'O') {
           board[i][j] = 'X';
         }
       }
+    }
+  }
+
+  private void dfs(char[][] board, int i, int j) {
+    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+      return;
+    }
+    board[i][j] = '#';
+    for (int[] direction : DIRECTIONS) {
+      dfs(board, i + direction[0], j + direction[1]);
     }
   }
 }
