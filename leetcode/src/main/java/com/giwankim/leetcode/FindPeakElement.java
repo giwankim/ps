bdaaf67d0127ce7d830f919bbf094f24aa461e57
Giwan Kim <giwan.kim@gmail.com>
2026-05-11 08:58:40 +0900

    refactor(leetcode): simplify find-peak-element with binary search
    - replace O(n) linear scan with O(log n) binary search by following the rising edge
    - document time and space complexity in Javadoc

--- leetcode/src/main/java/com/giwankim/leetcode/FindPeakElement.java
+++ leetcode/src/main/java/com/giwankim/leetcode/FindPeakElement.java
@@ -1,12 +1,21 @@
 package com.giwankim.leetcode;
 
 public class FindPeakElement {
+  /**
+   * <p>Time: {@code O(log n)}.
+   * <p>Space: {@code O(1)}.
+   */
   public int findPeakElement(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      if ((i == 0 || nums[i] > nums[i - 1]) && (i == nums.length - 1 || nums[i] > nums[i + 1])) {
-        return i;
+    int low = 0;
+    int high = nums.length - 1;
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] < nums[mid + 1]) {
+        low = mid + 1;
+      } else {
+        high = mid;
       }
     }
-    return -1;
+    return low;
   }
 }
