508f9847b0fcc3786581496373580aceab664880
Giwan Kim <giwan.kim@gmail.com>
2026-06-11 10:32:35 +0900

    test(cses): migrate Missing Number to TDD cases plus harness data tests

--- cses/src/test/java/cses/introductory/problems/MissingNumberTest.java
+++ cses/src/test/java/cses/introductory/problems/MissingNumberTest.java
@@ -1,44 +1,42 @@
 package cses.introductory.problems;
 
+import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
+import static support.Cses.run;
+import static support.Cses.tests;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import support.ResourceUtils;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 class MissingNumberTest {
-  @ParameterizedTest(name = "{index}")
-  @MethodSource
-  void missingNumber(Path input, Path output) throws IOException {
-    BufferedReader r = Files.newBufferedReader(input);
-    StringWriter out = new StringWriter();
-    PrintWriter pw = new PrintWriter(out);
-    String expected = Files.readString(output);
-
-    MissingNumber.missingNumber(r, pw);
-
-    assertThat(out).hasToString(expected);
+  @Test
+  void officialSampleFindsMissingMiddleValue() throws IOException {
+    assertThat(run(MissingNumber::missingNumber, "5\n2 3 1 5")).isEqualTo("4");
   }
 
-  private static Stream<Arguments> missingNumber() {
-    String prefix = "cses/missing/number/";
-    return IntStream.rangeClosed(1, 14).mapToObj(i -> {
-      try {
-        return Arguments.of(
-            ResourceUtils.getPath(prefix + i + ".in"), ResourceUtils.getPath(prefix + i + ".out"));
-      } catch (URISyntaxException e) {
-        throw new RuntimeException(e);
-      }
-    });
+  @Test
+  void smallestCaseMissingFirstValue() throws IOException {
+    assertThat(run(MissingNumber::missingNumber, "2\n2")).isEqualTo("1");
+  }
+
+  @Test
+  void smallestCaseMissingLastValue() throws IOException {
+    assertThat(run(MissingNumber::missingNumber, "2\n1")).isEqualTo("2");
+  }
+
+  @Test
+  void maxSizeSumOverflowsIntWithoutLong() throws IOException {
+    String numbers =
+        IntStream.rangeClosed(2, 200_000).mapToObj(Integer::toString).collect(joining(" "));
+    assertThat(run(MissingNumber::missingNumber, "200000\n" + numbers)).isEqualTo("1");
+  }
+
+  @TestFactory
+  Stream<DynamicTest> officialTestData() {
+    return tests("missing-number", MissingNumber::missingNumber);
   }
 }
