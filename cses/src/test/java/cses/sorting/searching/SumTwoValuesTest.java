d432a2694e6066cd726250c51b879a5372f6499e
Giwan Kim <giwan.kim@gmail.com>
2026-06-11 10:39:42 +0900

    test(cses): migrate Sum of Two Values to TDD cases plus harness data tests

--- cses/src/test/java/cses/sorting/searching/SumTwoValuesTest.java
+++ cses/src/test/java/cses/sorting/searching/SumTwoValuesTest.java
@@ -1,44 +1,38 @@
 package cses.sorting.searching;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static support.Cses.run;
+import static support.Cses.tests;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import support.ResourceUtils;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 class SumTwoValuesTest {
-  @ParameterizedTest(name = "{index}")
-  @MethodSource
-  void sumTwoValues(Path input, Path output) throws IOException {
-    BufferedReader r = Files.newBufferedReader(input);
-    StringWriter out = new StringWriter();
-    PrintWriter pw = new PrintWriter(out);
-    String expected = Files.readString(output);
-
-    SumTwoValues.sumTwoValues(r, pw);
-
-    assertThat(out).hasToString(expected);
+  @Test
+  void officialSampleFindsAPairSummingToTarget() throws IOException {
+    assertThat(run(SumTwoValues::sumTwoValues, "4 8\n2 7 5 1")).isEqualTo("4 2");
   }
 
-  private static Stream<Arguments> sumTwoValues() {
-    String prefix = "cses/sum/two/values/";
-    return IntStream.rangeClosed(1, 27).mapToObj(i -> {
-      try {
-        return Arguments.of(
-            ResourceUtils.getPath(prefix + i + ".in"), ResourceUtils.getPath(prefix + i + ".out"));
-      } catch (URISyntaxException e) {
-        throw new RuntimeException(e);
-      }
-    });
+  @Test
+  void duplicateValuesCanFormThePair() throws IOException {
+    assertThat(run(SumTwoValues::sumTwoValues, "4 6\n3 3 1 9")).isEqualTo("1 2");
+  }
+
+  @Test
+  void valueCannotPairWithItself() throws IOException {
+    assertThat(run(SumTwoValues::sumTwoValues, "3 4\n2 5 8")).isEqualTo("IMPOSSIBLE");
+  }
+
+  @Test
+  void singleValueIsImpossible() throws IOException {
+    assertThat(run(SumTwoValues::sumTwoValues, "1 2\n1")).isEqualTo("IMPOSSIBLE");
+  }
+
+  @TestFactory
+  Stream<DynamicTest> officialTestData() {
+    return tests("sum-of-two-values", SumTwoValues::sumTwoValues);
   }
 }
