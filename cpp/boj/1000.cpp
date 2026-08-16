4e36e8c84a85e82eabad5ad3e603f487673a7607
Giwan Kim <giwan.kim@gmail.com>
2026-08-16 11:53:28 +0900

    feat(cpp): discover solutions by glob and solve boj 1000

---
+++ cpp/boj/1000.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << a + b << '\n';
+  return 0;
+}
